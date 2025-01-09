559703efd4aa8b69d12b71bc8b0029cdb85fd53b
Hell0HighDefo <hell0hd454545@gmail.com>
2025-01-08 20:45:38 -0500

    carmapo anger mechanic
    currently broken, angertime does not change with punch

--- src/main/java/hell0hd/orangechin/entity/custom/CarmapoEntity.java
+++ src/main/java/hell0hd/orangechin/entity/custom/CarmapoEntity.java
@@ -2,13 +2,13 @@ package hell0hd.orangechin.entity.custom;
 
 
 import hell0hd.orangechin.entity.ModEntities;
-import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
-import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.ai.goal.*;
 import net.minecraft.entity.attribute.DefaultAttributeContainer;
-import net.minecraft.entity.attribute.EntityAttributeModifier;
 import net.minecraft.entity.attribute.EntityAttributes;
+import net.minecraft.entity.data.DataTracker;
+import net.minecraft.entity.data.TrackedData;
+import net.minecraft.entity.data.TrackedDataHandlerRegistry;
 import net.minecraft.entity.mob.Angerable;
 import net.minecraft.entity.mob.MobEntity;
 import net.minecraft.entity.passive.AnimalEntity;
@@ -16,9 +16,9 @@ import net.minecraft.entity.passive.PassiveEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
+import net.minecraft.nbt.NbtCompound;
 import net.minecraft.recipe.Ingredient;
 import net.minecraft.server.world.ServerWorld;
-import net.minecraft.util.Identifier;
 import net.minecraft.util.TimeHelper;
 import net.minecraft.util.math.intprovider.UniformIntProvider;
 import net.minecraft.world.World;
@@ -26,7 +26,12 @@ import org.jetbrains.annotations.Nullable;
 
 import java.util.UUID;
 
-public class CarmapoEntity extends AnimalEntity {
+public class CarmapoEntity extends AnimalEntity implements Angerable {
+    private static final TrackedData<Integer> ANGER_TIME = DataTracker.registerData(CarmapoEntity.class, TrackedDataHandlerRegistry.INTEGER);
+    private static final UniformIntProvider ANGER_TIME_RANGE = TimeHelper.betweenSeconds(20, 39);
+    @Nullable
+    private UUID angryAt;
+
     public CarmapoEntity(EntityType<? extends AnimalEntity> entityType, World world) {
         super(entityType, world);
     }
@@ -39,8 +44,12 @@ public class CarmapoEntity extends AnimalEntity {
         this.goalSelector.add(2, new TemptGoal(this, 1, Ingredient.ofItems(Items.WHEAT_SEEDS), false));
 
         this.goalSelector.add(3, new FollowParentGoal(this, 1));
-        this.goalSelector.add(4, new WanderAroundFarGoal(this, 1.0));
+        this.goalSelector.add(8, new WanderAroundFarGoal(this, 1.0));
         this.goalSelector.add(6, new LookAroundGoal(this));
+
+        this.goalSelector.add(5, new MeleeAttackGoal(this, 1.0, true));
+        this.targetSelector.add(8, new UniversalAngerGoal<>(this, true));
+        this.targetSelector.add(0, new ActiveTargetGoal<>(this, PlayerEntity.class, 10, true, false, this::shouldAngerAt));
 
     }
 
@@ -52,6 +61,60 @@ public class CarmapoEntity extends AnimalEntity {
                 .add(EntityAttributes.GENERIC_FOLLOW_RANGE, 1);
     }
 
+    protected void initDataTracker(DataTracker.Builder builder) {
+        super.initDataTracker(builder);
+        builder.add(ANGER_TIME, 0);
+    }
+
+    @Override
+    public void writeCustomDataToNbt(NbtCompound nbt) {
+        super.writeCustomDataToNbt(nbt);
+        this.writeAngerToNbt(nbt);
+    }
+
+    @Override
+    public void readCustomDataFromNbt(NbtCompound nbt) {
+        super.readCustomDataFromNbt(nbt);
+        this.readAngerFromNbt(this.getWorld(), nbt);
+    }
+
+    @Override
+    public void tickMovement() {
+        super.tickMovement();
+        if (!this.getWorld().isClient) {
+            this.tickAngerLogic((ServerWorld)this.getWorld(), true);
+        }
+    }
+
+    @Override
+    public int getAngerTime() {
+        return this.dataTracker.get(ANGER_TIME);
+    }
+
+    @Override
+    public void setAngerTime(int angerTime) {
+        this.dataTracker.set(ANGER_TIME, angerTime);
+    }
+
+    @Override
+    public void chooseRandomAngerTime() {
+        this.setAngerTime(ANGER_TIME_RANGE.get(this.random));
+    }
+
+    @Nullable
+    @Override
+    public UUID getAngryAt() {
+        return this.angryAt;
+    }
+
+    @Override
+    public void setAngryAt(@Nullable UUID angryAt) {
+        this.angryAt = angryAt;
+    }
+
+
+
+
     @Override
     public boolean isBreedingItem(ItemStack stack) {
         return stack.isOf(Items.WHEAT_SEEDS);
@@ -61,4 +124,12 @@ public class CarmapoEntity extends AnimalEntity {
     public @Nullable PassiveEntity createChild(ServerWorld world, PassiveEntity entity) {
         return ModEntities.CARMAPO.create(world);
     }
+
+
+
+
+
+
+
+
 }
