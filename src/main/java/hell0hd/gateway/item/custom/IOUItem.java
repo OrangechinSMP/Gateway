0165afd190eebbf6864a27b2b26e3960638271ef
Hell0HighDefo <hell0hd454545@gmail.com>
2025-06-10 15:48:30 -0400

    unused item

---
+++ src/main/java/hell0hd/gateway/item/custom/IOUItem.java
@@ -0,0 +1,30 @@
+package hell0hd.gateway.item.custom;
+
+import hell0hd.gateway.item.ModItems;
+import net.minecraft.enchantment.EnchantmentHelper;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.projectile.FishingBobberEntity;
+import net.minecraft.inventory.StackReference;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.tooltip.TooltipType;
+import net.minecraft.screen.slot.Slot;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.stat.Stats;
+import net.minecraft.text.Text;
+import net.minecraft.util.ClickType;
+import net.minecraft.util.Hand;
+import net.minecraft.util.TypedActionResult;
+import net.minecraft.world.World;
+import net.minecraft.world.event.GameEvent;
+
+import java.util.List;
+
+public class IOUItem extends Item {
+    public IOUItem(Settings settings) {
+        super(settings);
+    }
+}
