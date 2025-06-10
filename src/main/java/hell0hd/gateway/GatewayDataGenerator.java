dacb602e7580f9a9d5a098e36a487bfd5bddf58d
Hell0HighDefo <hell0hd454545@gmail.com>
2025-06-10 15:48:01 -0400

    Update GatewayDataGenerator.java

--- src/main/java/hell0hd/gateway/GatewayDataGenerator.java
+++ src/main/java/hell0hd/gateway/GatewayDataGenerator.java
@@ -6,6 +6,6 @@ import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 public class GatewayDataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
-
+		FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
 	}
 }
